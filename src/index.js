b8790599d71fc9ddfada87d18b28fb9e2440d19d
Scott Little <slittle@nulite-lighting.com>
2017-11-28 16:35:40 -0700

    fix assigning to string

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 export default function recursiveAssign(l, r) {
   for (const key in r) {
-    if (l[key] && Object.getPrototypeOf(r[key]) == Object.prototype) {
+    if (l[key] && l[key] instanceof Object && Object.getPrototypeOf(r[key]) == Object.prototype) {
       recursiveAssign(l[key], r[key])
     } else {
       l[key] = r[key]
